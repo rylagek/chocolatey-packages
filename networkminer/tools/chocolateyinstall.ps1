40d355244c17813ce9b3ffeb6b83834cf22d87d6
rylagek <thezak@protonmail.ch>
2022-02-13 08:28:42 -0600

    networkminer 2.7.2 packaged for chocolatey

---
+++ networkminer/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://www.netresec.com/?download=NetworkMiner'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP'
+  url            = $url
+
+  softwareName   = 'networkminer*'
+
+  checksum       = '094e14271a560a8880d3dded329c3d7cd9c84bdee527f926261c1b080a639b7f'
+  checksumType   = 'sha256'
+
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
